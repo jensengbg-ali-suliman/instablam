7bf610bfd7af2aa798d7f1edb2079f25659a55f6
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-05-26 20:01:55 +0200

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -5,11 +5,52 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    VideoStream: {},
+    brightness: 0,
+    saturation: 0,
+    contrast: 0,
+    vibrance: 0,
+    hue: 0,
   },
   mutations: {
+    saveVideoStream(state, stream) {
+      state.VideoStream = stream
+    },
+    uppdateBrightness(state, val) {
+      state.brightness = val
+    },
+    uppdateSaturation(state, val) {
+      state.saturation = val
+    },
+    uppdateContrast(state, val) {
+      state.contrast = val
+    },
+    uppdateVibrance(state, val) {
+      state.vibrance = val
+    },
+    uppdateHue(state, val) {
+      state.hue = val
+    },
   },
   actions: {
+    getVideoStream(ctx, stream) {
+      ctx.commit('saveVideoStream', stream)
+    },
+    uppdateBrightness(ctx, val) {
+      ctx.commit('uppdateBrightness', val)
+    },
+    uppdateSaturation(ctx, val) {
+      ctx.commit('uppdateSaturation', val)
+    },
+    uppdateContrast(ctx, val) {
+      ctx.commit('uppdateContrast', val)
+    },
+    uppdateVibrance(ctx, val) {
+      ctx.commit('uppdateVibrance', val)
+    },
+    uppdateHue(ctx, val) {
+      ctx.commit('uppdateHue', val)
+    },
   },
-  modules: {
-  }
+  modules: {},
 })
