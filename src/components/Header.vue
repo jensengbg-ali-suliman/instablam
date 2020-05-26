2ffa9142dfbf0e306c14c94f6564891cfa7b52b8
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-05-26 20:01:18 +0200

    Create Filters.vue

---
+++ src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <span class="logo">instablam</span>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+header {
+  position: relative;
+  background: #000;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  min-height: 5rem;
+  font-family: "Lobster", cursive;
+  color: #efefef;
+}
+</style>
