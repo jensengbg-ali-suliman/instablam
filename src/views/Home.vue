2c6a38a0e4f47b15fddfd210106590a1944a5976
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-05-26 20:02:01 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header></Header>
+    <video-stream />
+    <footer>
+      <div class="circle"></div>
+      <button id="cta" @click="takePicture"></button>
+    </footer>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
+import Header from "../components/Header.vue";
+import VideoStream from "../components/VideoStream.vue";
 export default {
-  name: 'Home',
   components: {
-    HelloWorld
+    Header,
+    VideoStream
+  },
+  methods: {
+    takePicture: function() {
+      this.$router.push("/image");
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  background: #222;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-content: center;
+
+  .circle {
+    background: #c4c4c4;
+    width: 1rem;
+    height: 1rem;
+    border-radius: 50%;
+    position: absolute;
+    left: 3rem;
+  }
+
+  footer {
+    position: relative;
+    background: #000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    min-height: 6rem;
   }
 }
-</script>
+
+@media only screen and (min-width: 768px) {
+  .home {
+    width: 768px;
+    margin: 0 auto;
+  }
+}
+</style>
