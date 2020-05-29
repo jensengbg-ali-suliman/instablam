f0f57da3da558b0386a15678704d7624cd0961e6
jensen-ali-suliman <alxso@LAPTOP-92UALMD9.(none)>
2020-05-29 08:46:56 +0200

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,5 @@
 self.addEventListener('instal', (e) => {
+  e.waitUntil(caches.open('v1').then((cache) => cache.addAll(['offline.html'])))
   self.skipWaiting()
   console.log('SW has been installed at: ', new Date().toLocaleTimeString())
 })
@@ -9,5 +10,24 @@ self.addEventListener('activate', (e) => {
 })
 
 self.addEventListener('fetch', (e) => {
-  console.log(e.request)
+  if (!navigator.onLine) {
+    e.respondWith(
+      caches.match(e.request).then((res) => {
+        if (res) {
+          return res
+        } else {
+          return caches.match(new Request('offline.html'))
+        }
+      })
+    )
+  } else {
+    upploadDataToCache(e.request)
+  }
 })
+
+async function upploadDataToCache(req) {
+  const res = await fetch(req)
+  const cache = await caches.open('v1')
+  const data = await cache.put(req, res.clone())
+  return res
+}
